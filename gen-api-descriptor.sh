45c19ffd6fe9d22f3d871e3feba99a73e40f182b
wahyudotdev <wahyudotdev@gmail.com>
2023-05-01 19:40:31 +0700

    fix: adding field_behavior.proto

--- gen-api-descriptor.sh
+++ gen-api-descriptor.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# you must always include the annotations.proto, descriptor.proto, and http.proto
+# You must always include all proto files that included inside service definition
 # otherwise Envoy will always fail to read the descriptor
+# In this case, annotations.proto, http.proto, and field_behavior.proto must be included
 protoc -I ./proto \
    --descriptor_set_out=./envoy/api_descriptor.pb \
    ./proto/bookstore/bookstore.proto \
    ./proto/cart/cart.proto \
    ./proto/google/api/annotations.proto \
    ./proto/google/api/http.proto \
-   ./proto/google/protobuf/descriptor.proto
+   ./proto/google/protobuf/descriptor.proto \
+   ./proto/google/api/field_behavior.proto
